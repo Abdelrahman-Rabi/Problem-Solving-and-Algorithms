ec2c825257d13b8b4c58bdde335e3b6fe92f7608
your_github_username <rabi.abdelrahman@gmail.com>
2020-12-12 10:00:40 +0200

    Q20 Pseudo code

--- Q20/main.js
+++ Q20/main.js
@@ -1,10 +1,13 @@
-console.log('Problem Solving Q: 20');
+console.log("Problem Solving Q: 20");
 
 /* largestProductOfThree */
 
 /*  
 Write a function that accepts an array of integers and returns the largest product possible from three of those numbers.
 */
+// Pseudo code
+// => find the largest three num in the array and push them in new array
+// then return the multi of the three num in the new array
 
 function largestProductOfThree() {
   // YOUR CODE HERE
