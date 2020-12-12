cf7ef9ea92a78d8c1e0d14d55ddcf2b54b4d892f
your_github_username <rabi.abdelrahman@gmail.com>
2020-12-12 03:03:51 +0200

    Q16 Pseudo code

--- Q16/main.js
+++ Q16/main.js
@@ -1,10 +1,20 @@
-console.log('Problem Solving Q:16 ');
+console.log("Problem Solving Q:16 ");
 
 /* evenOccurrence */
 
 /*  
 Find the first item that occurs an even number of times in an array.
 */
+
+// Pseudo code
+// => check the repeated numbers
+// => check the index of each repeted number
+// => the minimum index for both values will be occurs twice first
+// ex. => arr = [1,9,7,3,6,6,8,9,9]
+//      index    0 1 2 3 4 5 6 7 8
+//  for num 9 index sum 1,7,8
+//  for num 6 index sum 4,5
+//  5<7 so num 6 occurs twice first
 
 function evenOccurrence() {
   // YOUR CODE HERE
