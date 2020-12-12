f14074880008ccc1641c4a5e31a797b1d66bf012
your_github_username <rabi.abdelrahman@gmail.com>
2020-12-12 03:09:53 +0200

    Q17 Pseudo code

--- Q17/main.js
+++ Q17/main.js
@@ -1,10 +1,14 @@
-console.log('Problem Solving Q:17 ');
+console.log("Problem Solving Q:17 ");
 
 /*  */
 
 /*  
 Given an arbitrary input string, return the first non-repeating character. For strings with all repeats, return 'sorry'
 */
+
+// Pseudo code
+// convert string to array
+// taking each element through array, check if elemnt repeated if No return first elemnt
 
 function firstNonRepeatedCharacter() {
   // YOUR CODE HERE
