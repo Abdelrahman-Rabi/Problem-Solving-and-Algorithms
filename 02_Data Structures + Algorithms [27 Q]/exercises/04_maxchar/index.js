ae7c25beeabec7a0f571276e72f2fb9669e240ed
your_github_username <rabi.abdelrahman@gmail.com>
2020-12-27 01:05:52 +0200

    Q4 solved

--- 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  arr = str.split("");
+  obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] >= 1) {
+      obj[arr[i]] = obj[arr[i]] + 1;
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+  const max = Math.max(...Object.values(obj));
+  return Object.keys(obj).find((key) => obj[key] === max);
+}
 
 module.exports = maxChar;
