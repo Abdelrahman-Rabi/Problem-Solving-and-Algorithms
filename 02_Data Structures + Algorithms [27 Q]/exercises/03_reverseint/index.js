0bc3eeae2916d1e182217f653c1ec7bc76c09f75
your_github_username <rabi.abdelrahman@gmail.com>
2020-12-26 19:37:53 +0200

    Q3 solved

--- 02_Data Structures + Algorithms [27 Q]/exercises/03_reverseint/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/03_reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  arr = n.toString().split("");
+  let result = [];
+  for (let i = arr.length; i >= 0; i--) {
+    result.push(arr[i]);
+  }
+  if (n >= 0) {
+    return parseInt(result.join(""));
+  } else {
+    return parseInt(result.join("")) * -1;
+  }
+}
 
 module.exports = reverseInt;
