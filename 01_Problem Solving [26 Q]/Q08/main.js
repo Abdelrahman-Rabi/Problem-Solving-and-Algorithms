d5b2026b998e0df8499460e0453af50ba3da20cc
your_github_username <rabi.abdelrahman@gmail.com>
2021-01-12 09:45:14 +0200

    Q08 Solved

--- 01_Problem Solving [26 Q]/Q08/main.js
+++ 01_Problem Solving [26 Q]/Q08/main.js
@@ -7,13 +7,23 @@ Write a function that returns the number of unique items in an array.
 */
 
 function uniqueItems(arr) {
-  let uniqueArr = [];
+  let result = [];
   for (let i = 0; i < arr.length; i++) {
-    if (!uniqueArr.includes(arr[i])) {
-      uniqueArr.push(arr[i]);
+    if (result.indexOf(arr[i]) == -1) {
+      result.push(arr[i]);
     }
   }
-  return uniqueArr.length;
+  return result.length;
+}
+
+function uniqueItems(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result.length;
 }
 
 /* 
@@ -26,3 +36,23 @@ uniqueItems([1, 2, 3, 1, 2, 3]) // => 3
 uniqueItems([5, 1, 2, 2, 1, 5]) // => 3
 uniqueItems([5, 77, 1, 3]) // => 4
 */
+
+function uniqueItems(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (result.indexOf(arr[i]) == -1) {
+      result.push(arr[i]);
+    }
+  }
+  return result.length;
+}
+
+function uniqueItems(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result.length;
+}
