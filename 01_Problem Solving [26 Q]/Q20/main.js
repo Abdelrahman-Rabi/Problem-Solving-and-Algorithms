7a021b0a10e3a7eefcaddbc3189c5f8c2485a145
your_github_username <rabi.abdelrahman@gmail.com>
2021-01-14 20:08:22 +0200

    Q20 Solved

--- 01_Problem Solving [26 Q]/Q20/main.js
+++ 01_Problem Solving [26 Q]/Q20/main.js
@@ -1,4 +1,4 @@
-console.log('Problem Solving Q: 20');
+console.log("Problem Solving Q: 20");
 
 /* MaximumProductOfThree */
 
@@ -6,8 +6,16 @@ console.log('Problem Solving Q: 20');
 Write a function that accepts an array of integers and returns the Maximum product possible from three of those numbers.
 */
 
-function MaximumProductOfThree() {
-  // YOUR CODE HERE
+function MaximumProductOfThree(arr) {
+  let maxArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let maxNum = Math.max(...arr);
+    maxArr.push(maxNum);
+    if (maxArr.length === 3) {
+      return maxArr[0] * maxArr[1] * maxArr[2];
+    }
+  }
+  return "error";
 }
 
 /* 
