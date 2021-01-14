e3f71383c9fd758963119db967d9aa65f7593b48
your_github_username <rabi.abdelrahman@gmail.com>
2021-01-14 19:05:51 +0200

    Q17 Solved

--- 01_Problem Solving [26 Q]/Q17/main.js
+++ 01_Problem Solving [26 Q]/Q17/main.js
@@ -1,4 +1,4 @@
-console.log('Problem Solving Q:17 ');
+console.log("Problem Solving Q:17 ");
 
 /* findFirstNonIterativedCharacter */
 
@@ -6,8 +6,15 @@ console.log('Problem Solving Q:17 ');
 Given an arbitrary input string, return the first Non Iteratived character. For strings with all repeats, return 'sorry'
 */
 
-function findFirstNonIterativedCharacter() {
-  // YOUR CODE HERE
+function findFirstNonIterativedCharacter(str) {
+  const arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    let result = arr.shift();
+    if (!arr.includes(result)) {
+      return result;
+    }
+  }
+  return "Sorry";
 }
 
 /* 
